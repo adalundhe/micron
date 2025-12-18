66dec88c5cc1884ec3957c960150009eb693a55a
Ada Lundhe <sean.corbett@umontana.edu>
2025-12-18 08:58:19 -0600

    AL: fix package name

--- middleware/sso/sso.go
+++ middleware/sso/sso.go
@@ -1,13 +1,20 @@
 package sso
 
 import (
+	"github.com/adalundhe/micron/auth"
 	"github.com/adalundhe/micron/config"
 	"github.com/adalundhe/micron/internal/provider"
 )
 
 
-func CreateSSOMiddlewareAndHandlers(cfg *config.Config, opts *provider.SSOOpts) (provider.SSO, error) {
-	provider, err := provider.NewSSOProvider(cfg.Providers.SSO, cfg.Api, opts)
+
+func CreateSSOMiddlewareAndHandlers(
+	cfg *config.Config, 
+	factory func () auth.SSOClaims,
+) (provider.SSO, error) {
+	provider, err := provider.NewSSOProvider(cfg.Providers.SSO, cfg.Api, &provider.SSOOpts{
+		CreateClaims: factory,
+	})
 	if err != nil {
 		return nil, err
 	}
